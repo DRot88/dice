bd5edf84d4374b4b75f22f740e5742c6948077bf
Drot88 <Daniel.Rotenberg88@gmail.com>
2019-06-09 18:31:47 -0500

    Created basic dice component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dice from './Dice'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Dice />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Dice.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Dice extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div>
+                <h1>Dice!</h1>
+            </div>
+        )
+    }
+}
+
+export default Dice;
